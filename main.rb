e3dfe90aaffcb3765c704732c921b3ceab2e48db
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-09-13 14:28:12 +0300

    OOP school library decorate a class

---
+++ main.rb
@@ -0,0 +1,10 @@
+require_relative 'person.rb'
+require_relative 'capitalize_decorator.rb'
+require_relative 'trimmer_decorator.rb'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
